d122fec6697b6c84f2ffb3a88cdcfac4d7e00772
Jonah <jydelaware@gmail.com>
2023-10-09 14:10:47 -0400

    services section

--- src/main/webapp/javascript/Home.jsx
+++ src/main/webapp/javascript/Home.jsx
@@ -7,9 +7,74 @@ export default function Home(){
         <div>
             <section className="introduction">
                 <div>
-                    <h1>Welcome to All Pro</h1>
-                    <p>Full service pro audio electronics repair located in central Ohio</p>
+                    <h1>All Pro</h1>
+                    <p>Full service consumer and pro audio electronics repair located in Columbus Ohio</p>
+                    <ul>
+                        <li> Audio Equipment</li>
+                        <li> Consumer Electronics</li>
+                        <li> Service Calls</li>
+                    </ul>
                 </div>
+                <div>
+                    <img src="images/marantz.webp" alt="audio receiver"/>
+                </div>
+            </section>
+            <section className="service-section">
+                <h2>Service Offerings</h2>
+                <div className="services">
+                    <div>
+                        <img src="images/guitar-amp.png" alt="fender guitar amplifier"/>
+                        <details>
+                            <summary>Music Gear</summary>
+                            <ul>
+                                <li>Instrument Amplifiers</li>
+                                <li>Microphones</li>
+                                <li>Keyboards</li>
+                                <li>FX Pedals</li>
+                            </ul>
+                        </details>
+                    </div>
+
+                    <div>
+                        <img src="images/tv.webp" alt="flat screen television"/>
+                        <details>
+                            <summary>Consumer Electronics</summary>
+                            <ul>
+                                <li>TVs</li>
+                                <li>Media Players (DVD, CD, Etc.)</li>
+                                <li>Arcade Cabinets</li>
+                                <li>Radios</li>
+                            </ul>
+                        </details>
+                    </div>
+
+                    <div>
+                        <img src="images/vintage-stereo.png" alt="Vintage Sony amplifier"/>
+                        <details>
+                            <summary>Vintage Audio</summary>
+                            <ul>
+                                <li>Audio Receivers</li>
+                                <li>Turntables</li>
+                                <li>Tape Machines</li>
+                                <li>Speakers</li>
+                            </ul>
+                        </details>
+                    </div>
+
+                    <div>
+                        <img src="images/mixer.webp" alt="Yamaha mixing board"/>
+                        <details>
+                            <summary>Live/Studio Audio Gear</summary>
+                            <ul>
+                                <li>Mixers</li>
+                                <li>Power Amps</li>
+                                <li>PA Speakers</li>
+                                <li>DJ Controllers</li>
+                            </ul>
+                        </details>
+                    </div>
+                </div>
+                <p>Don't see your repair need listed? Let's get in touch!</p>
             </section>
         </div>
     )
--- src/main/webapp/javascript/Main.jsx
+++ src/main/webapp/javascript/Main.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
@@ -12,12 +13,29 @@ function Layout() {
         return (
         <>
             <header className="header">
-                <div className="logo">
-                
-                </div>
-                <nav className="navigation">
-                    <Link to="/">Home</Link>
-                    <Link to="/Contact">Contact</Link>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">
+                        <img src="images/AllProLogo.png"/>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Rates</a>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </header>
             <Outlet />
