c9c63d44cef1deb9414e647d2ee18bde321d4256
Jonah <jydelaware@gmail.com>
2023-10-18 17:36:59 -0400

    add about.jsx

---
+++ src/main/webapp/javascript/About.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import "../css/style.css";
+
+export default function About(){
+    return(
+        <div>
+            <p>
+                All Pro Electronic Service was founded by Chris Young in 2009. With over 30 years of industry experience in the electronics repair, 
+                Chris has turned All Pro into Columbus, Ohio's premier destination for top-tier electronics repairs and installations, catering to both consumers and businesses.
+                From guitar amplifiers to televisions, if parts can be found, it can likely be fixed. 
+            </p>
+        </div>
+    )
+}
